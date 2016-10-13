ceb301f9464b391f5aa85840f3665a256ea43a0e
Hasnaat Khan <khah1_15@uni.worc.ac.uk>
2016-10-13 14:25:42 +0100

    Website

---
+++ www/slider.js
@@ -0,0 +1,27 @@
+$(function () {
+    
+
+    var change_img_time 	= 3000;	
+    var transition_speed	= 300;
+    
+    var simple_slideshow	= $("#Slider"),
+        listItems 			= simple_slideshow.children('li'),
+        listLen				= listItems.length,
+        i 					= 0,
+		
+        changeList = function () {
+		
+			listItems.eq(i).fadeOut(transition_speed, function () {
+				i += 1;
+				if (i === listLen) {
+					i = 0;
+				}
+				listItems.eq(i).fadeIn(transition_speed);
+			});
+
+        };
+		
+    listItems.not(':first').hide();
+    setInterval(changeList, change_img_time);
+	
+});
